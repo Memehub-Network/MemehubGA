6c6e44fdf1cb3fb006f9240b362070c4aa6d72de
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-10 09:53:49 +0300

    Update App.java

--- app/src/main/java/com/memehub/network/App.java
+++ app/src/main/java/com/memehub/network/App.java
@@ -1,1 +1,22 @@
+package com.memehub.network;
 
+import android.app.AlarmManager;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Process;
+import android.util.Log;
+
+public class App extends Application {
+  
+  private static Context context;
+  public static Context getContext() {
+    return context;
+  }
+  
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+  
+}
